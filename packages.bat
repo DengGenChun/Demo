1b774760db81fd6b496b209e0b8cd146b73d695b
denggenchun <cdenggc@gmail.com>
2018-09-07 00:19:02 +0800

    增加数据库迁移

--- packages.bat
+++ packages.bat
@@ -5,5 +5,7 @@ set PATH=%PYTHON_HOME%;%PYTHON_HOME%\Scripts;%PATH%
 pip install pipenv
 pipenv install flask
 pipenv install flask_sqlalchemy
+pipenv install flask_script
+pipenv install flask_migrate
 pipenv install pymysql
 pipenv install requests
